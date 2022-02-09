b7fb4791e565e4070b087a3b10946babf9e35294
Paul Joachim <pauljoachim@web.de>
2022-02-09 13:36:32 +0100

    Fix API and add login functions

--- disallowed/model/user.php
+++ disallowed/model/user.php
@@ -22,18 +22,45 @@ class User {
 
     // fet data from database using email since its unique
     public function fetch() {
+        $sql = new SQL();
 
+        $result = $sql->sql_request("SELECT * FROM Benutzer WHERE EMail='$this->email'");
+
+        $this->userid = $result->get_from_column("BenutzerID");
+        $this->forename = $result->get_from_column("Vorname");
+        $this->surname = $result->get_from_column("Name");
+        $this->phone = $result->get_from_column("Telefon");
+        $this->residence = $result->get_from_column("Ort");
+        $this->postal = $result->get_from_column("PLZ");
+        $this->street = $result->get_from_column("Strasse");
+        $this->house = $result->get_from_column("Hausnummer");
     }
 
     // Store data in database
-    // If $passwordhash is null, it won't be stored
-    public function store_data(string $passwordhash = null) {
+    public function store_data() {
 
     }
 
-    // TODO
+    public static function create_account(string $email, string $clearpassword) {
+        $sql = new SQL();
+        if ($sql->sql_request("SELECT * FROM Benutzer WHERE EMail='$email'")->get_num_rows() !== 1) {
+            return false;
+        }
+
+        $hash = password_hash($clearpassword, PASSWORD_DEFAULT);
+        $sql->sql_request("INSERT INTO Benutzer (BerechtigungsID, EMail, PasswordHash) VALUES (1, '$email', '$hash')");
+        return true;
+    }
+
     public static function verify_password(string $email, string $clearpassword) {
-        $hash = password_hash("12345", PASSWORD_DEFAULT);
+        $sql = new SQL(false);
+        $result = $sql->sql_request("SELECT PasswordHash FROM Benutzer WHERE EMail='$email'");
+        
+        if ($result->get_num_rows() !== 1) {
+            return false;
+        }
+
+        $hash = $result->get_from_column("PasswordHash");
         return password_verify($clearpassword, $hash);
     }
 
--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
+declare(strict_types = 1);
 
 session_start();
 
 require "disallowed/authenticate.php";
 require_once "disallowed/model/database/SQL.php";
 
-if (is_loggedin()) {
-    logout();
-} else {
-    login("jens.rosenbauer@ohgw.de", "12345");
-}
+#------------- Actual start -------------#
 
 $xml = new SimpleXMLElement("<xml/>");
 
 $xml->addChild('xslcontent', 'mainpage');
 $xml->addChild('content', 'Email: '.(isset($_SESSION['user']) ? $_SESSION['user']->email : "not logged in"));
-$xml->addChild('loggedin', is_loggedin());
+$xml->addChild('loggedin', strval(is_loggedin()));
 
 $xsl = new DOMDocument();
 $xsl->load("disallowed/xsl/base.xsl");
