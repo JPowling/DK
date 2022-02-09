b12cd5a66c98447b09dc7a11f1f8f281c0a569c8
Paul Joachim <pauljoachim@web.de>
2022-02-09 20:44:03 +0100

    Fixing script failing to load base

--- index.php
+++ index.php
@@ -6,15 +6,13 @@ session_start();
 require_once "disallowed/authenticate.php";
 require_once "disallowed/backend/database/SQL.php";
 
-$xsl_ns = "http://www.w3.org/1999/XSL/Transform";
-
 #------------- Actual start -------------#
 
 if (isset($_GET["route"]) && $_GET["route"] !== "") {
     $filename_php = "disallowed/scripts/".$_GET["route"].".php";
     $filename_xsl = "disallowed/xsl/".$_GET["route"].".xsl";
 
-    if (file_exists($filename_xsl)) {
+    if (file_exists($filename_xsl) && $_GET["route"] !== "base") {
         $xslcontent = $_GET["route"];
         
         // If the php file exists, run it
