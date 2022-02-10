a8a7925481c2c3e16733e94493f6c29df37830af
Paul Joachim <pauljoachim@web.de>
2022-02-10 17:20:07 +0100

    readd files

---
+++ disallowed/backend/database/sql.php
@@ -0,0 +1,24 @@
+<?php
+# Jens
+
+require_once("disallowed/backend/database/db.php");
+require_once("disallowed/backend/database/result.php");
+
+class SQL
+{
+    private PDO $pdo;
+
+    public function __construct(bool $is_w = false)
+    {
+        $this->pdo = DB::connect($is_w);
+    }
+
+
+    public function sql_request(String $command = ";")
+    {
+        $cmd = $this->pdo->prepare($command);
+        $cmd->execute();
+        $result = $cmd->fetchAll();
+        return new Result($result);
+    }
+}
